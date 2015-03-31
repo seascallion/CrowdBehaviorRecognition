7e0a1159a4be92e3756a894fd4c11c4b57d2bb6b
Sam Adams <samadams532@gmail.com>
2015-03-31 14:51:36 -0500

    new experimentation with gabor and fourier stuff

--- src/main.py
+++ src/main.py
@@ -2,11 +2,39 @@ from cv2 import *
 from matplotlib.pyplot import *
 
 im = imread('../data/images/crowd.jpg')
-edges = Canny(im, 100, 200)
 
-subplot(121), imshow(im, cmap='gray')
+subplot(221), imshow(im, cmap='gray')
 title('Original Image'), xticks([]), yticks([])
-subplot(122), imshow(edges, cmap='gray')
+
+# Canny edges
+edges = Canny(im, 200, 255)
+subplot(222), imshow(edges, cmap='gray')
 title('Edges'), xticks([]), yticks([])
+
+# Gabor segmentation
+params = {
+    'ksize': (100, 100),
+    'sigma': 1.0,
+    'theta': 3.14 / 2,
+    'lambd': 15.0,
+    'gamma': 0.02,
+    #'psi': 0,
+    'ktype': CV_32F
+}
+kern = getGaborKernel(**params)
+
+result = filter2D(im, CV_8UC3, kern)
+
+subplot(223), imshow(result, cmap='gray')
+title('Segmented'), xticks([]), yticks([])
+
+# Fourier transform
+f = np.fft.fft2(im)
+fshift = np.fft.fftshift(f)
+mag_spec = np.log(np.abs(fshift))
+mag_spec *= 255.0 / mag_spec.max()
+
+subplot(224), imshow(mag_spec, cmap='gray')
+title('Magnitude Spectrum'), xticks([]), yticks([])
 
 show()
