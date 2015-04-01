796ed73074a057d5f0ac5a238d09231cc366ec6e
Sam Adams <samadams532@gmail.com>
2015-04-01 00:48:31 -0500

    trying a method of 'cheeringness', which uses the sobel filter to measure the amount of vertical edges in the image, indicating the amount of cheering that is likely happening

--- src/main.py
+++ src/main.py
@@ -1,51 +1,48 @@
 from cv2 import *
 from matplotlib.pyplot import *
 
-im = imread('../data/images/crowd.jpg')
 
-subplot(241), imshow(im, cmap='gray')
+def mag_spec(im):
+    # Fourier transform
+    f = np.fft.fft2(im)
+    fshift = np.fft.fftshift(f)
+    result = np.log(np.abs(fshift))
+    result *= 255.0 / result.max()
+    return result
+
+im = imread('../data/images/crowd4.jpg')
+im = cvtColor(im, COLOR_RGB2GRAY)
+
+subplot(221), imshow(im, cmap='gray')
 title('Original Image'), xticks([]), yticks([])
 
 # Canny edges
-edges = Canny(im, 200, 255)
-subplot(242), imshow(edges, cmap='gray')
-title('Edges'), xticks([]), yticks([])
+#edges = Canny(im, 200, 255)
 
-# Gabor segmentation
-params = {
-    'ksize': (100, 100),
-    'sigma': 1.0,
-    'theta': 3.14 / 2,
-    'lambd': 15.0,
-    'gamma': 0.02,
-    #'psi': 0,
-    'ktype': CV_32F
-}
-kern = getGaborKernel(**params)
+#subplot(222), imshow(edges, cmap='gray')
+#title('Magnitude Spectrum'), xticks([]), yticks([])
 
-result = filter2D(im, CV_8UC3, kern)
+ksize = 5
 
-subplot(243), imshow(result, cmap='gray')
-title('Segmented'), xticks([]), yticks([])
+sobel_x = np.abs(Sobel(im, CV_64F, 1, 0, ksize=ksize))
+sobel_y = np.abs(Sobel(im, CV_64F, 0, 1, ksize=ksize))
 
-# Fourier transform
-f = np.fft.fft2(edges)
-fshift = np.fft.fftshift(f)
-mag_spec = np.log(np.abs(fshift))
-mag_spec *= 255.0 / mag_spec.max()
-
-subplot(244), imshow(mag_spec, cmap='gray')
-title('Magnitude Spectrum'), xticks([]), yticks([])
-
-im = GaussianBlur(mag_spec, ksize=(301, 301), sigmaX=50, sigmaY=50, borderType=BORDER_REFLECT)
-sobel_y = Sobel(im, CV_64F, 0, 1, ksize=31)
-
+sobel_x *= 255.0 / sobel_x.max()
 sobel_y *= 255.0 / sobel_y.max()
 
-subplot(245), imshow(im, cmap='gray')
-title('Blurred Result'), xticks([]), yticks([])
+#edge_mag_spec = mag_spec(edges)
 
-subplot(246), imshow(sobel_y, cmap='gray')
-title('Sobel of That'), xticks([]), yticks([])
+subplot(223), imshow(sobel_x, cmap='gray')
+title('Sobel X'), xticks([]), yticks([])
+
+subplot(224), imshow(sobel_y, cmap='gray')
+title('Sobel Y'), xticks([]), yticks([])
+
+vert_amount = sobel_x.mean()
+horz_amount = sobel_y.mean()
+
+cheeringness = vert_amount / horz_amount
+
+print 'Cheeringness:', cheeringness
 
 show()
