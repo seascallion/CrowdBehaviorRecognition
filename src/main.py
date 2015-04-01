aa67bfc3d85d0cb4446e6a04a05377a51af7354d
Sam Adams <samadams532@gmail.com>
2015-04-01 00:56:39 -0500

    added more images, removed unused code

--- src/main.py
+++ src/main.py
@@ -1,26 +1,15 @@
 from cv2 import *
 from matplotlib.pyplot import *
 
+filename = 'crowd4.jpg'
 
-def mag_spec(im):
-    # Fourier transform
-    f = np.fft.fft2(im)
-    fshift = np.fft.fftshift(f)
-    result = np.log(np.abs(fshift))
-    result *= 255.0 / result.max()
-    return result
+directory = '../data/images'
 
-im = imread('../data/images/crowd4.jpg')
+im = imread(directory + '/' + filename)
 im = cvtColor(im, COLOR_RGB2GRAY)
 
 subplot(221), imshow(im, cmap='gray')
 title('Original Image'), xticks([]), yticks([])
-
-# Canny edges
-#edges = Canny(im, 200, 255)
-
-#subplot(222), imshow(edges, cmap='gray')
-#title('Magnitude Spectrum'), xticks([]), yticks([])
 
 ksize = 5
 
@@ -29,8 +18,6 @@ sobel_y = np.abs(Sobel(im, CV_64F, 0, 1, ksize=ksize))
 
 sobel_x *= 255.0 / sobel_x.max()
 sobel_y *= 255.0 / sobel_y.max()
-
-#edge_mag_spec = mag_spec(edges)
 
 subplot(223), imshow(sobel_x, cmap='gray')
 title('Sobel X'), xticks([]), yticks([])
@@ -41,8 +28,9 @@ title('Sobel Y'), xticks([]), yticks([])
 vert_amount = sobel_x.mean()
 horz_amount = sobel_y.mean()
 
-cheeringness = vert_amount / horz_amount
+cheeringness = np.log(vert_amount / horz_amount)
 
+print 'File:', filename
 print 'Cheeringness:', cheeringness
 
 show()
