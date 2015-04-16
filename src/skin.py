7fe3ffa50ae5eb54f7d121f74c91d87aa8c7a06f
Sam Adams <samadams3@me.com>
2015-04-16 00:09:46 -0500

    removed some krap

--- src/skin.py
+++ src/skin.py
@@ -1,41 +1,34 @@
+# import the necessary packages
 import numpy as np
-from cv2 import getStructuringElement, erode, dilate, cvtColor, inRange, GaussianBlur, bitwise_and, COLOR_BGR2HSV, MORPH_ELLIPSE
+import cv2
 
 # define the upper and lower boundaries of the HSV pixel
 # intensities to be considered 'skin'
-# TODO: figure out what range to use
-# AND figure out why this doesn't work
-lower = np.array([0, 0, 0], dtype='uint8')
-upper = np.array([255, 255, 255], dtype='uint8')
+lower = np.array([0, 48, 60], dtype = "uint8")
+upper = np.array([20, 255, 255], dtype = "uint8")
 
-def detect_skin(im):
-	# resize the frame, convert it to the HSV color space,
-	# and determine the HSV pixel intensities that fall into
-	# the specified upper and lower boundaries
-	converted = cvtColor(im, COLOR_BGR2HSV)
-	skinMask = inRange(converted, lower, upper)
-	
-	min = [100,100,100]
-	max = [100,100,100]
-	for row in im:
-		for col in row:
-			for i in range(3):
-				if col[i] < min[i]:
-					min[i] = col[i]
-				if col[i] > max[i]:
-					max[i] = col[i]
-	print min
-	print max
+# grab the image
+imagePath = 'C:\GithubRepos\CrowdBehaviorRecognition\data\images\crowd14.jpg'
+oImage = cv2.imread(imagePath)
+image = cv2.resize(oImage, (0,0), fx=2, fy=2)
 
-	# apply a series of erosions and dilations to the mask
-	# using an elliptical kernel
-	#kernel = getStructuringElement(MORPH_ELLIPSE, (11, 11))
-	#skinMask = erode(skinMask, kernel, iterations = 2)
-	#skinMask = dilate(skinMask, kernel, iterations = 2)
+# resize the frame, convert it to the HSV color space,
+# and determine the HSV pixel intensities that fall into
+# the speicifed upper and lower boundaries
+converted = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+skinMask = cv2.inRange(converted, lower, upper)
 
-	# blur the mask to help remove noise, then apply the
-	# mask to the frame
-	#skinMask = GaussianBlur(skinMask, (3, 3), 0)
-	#skin = bitwise_and(im, im, mask=skinMask)
-	
-	return skinMask
+# apply a series of erosions and dilations to the mask
+# using an elliptical kernel
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 7))
+skinMask = cv2.erode(skinMask, kernel, iterations = 2)
+skinMask = cv2.dilate(skinMask, kernel, iterations = 2)
+
+# blur the mask to help remove noise, then apply the
+# mask to the frame
+skinMask = cv2.GaussianBlur(skinMask, (3, 3), 0)
+skin = cv2.bitwise_and(image, image, mask = skinMask)
+
+# show the skin in the image along with the mask
+cv2.imshow("images", np.hstack([image, skin]))
+cv2.waitKey(0)
