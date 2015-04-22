b199ab63323324884afe9e789ac8827082f29061
Tom McClenahan <tom@mcclenahan.me>
2015-04-21 22:53:10 -0500

    Show results for density estimation

--- src/main.py
+++ src/main.py
@@ -1,21 +1,40 @@
+import cv2
+import csv
 from cv2 import imread, Sobel, CV_64F
 from matplotlib.pyplot import title, xticks, yticks, subplot, imshow, show
 from skin import detect_skin
 import numpy as np
 from os import listdir
 
-display_filenames = ['043.jpg']
+display_filenames = []
+test_results = dict()
+
+cheer_threshold = 0.1
+density_low_threshold = 0.1
+density_high_threshold = 0.18
 
 directory = '../data/images'
+with open('../data/csv.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        test_filename = row['file']
+        test_results[test_filename] = row
+
+density_dict = {
+    'low': [0, 0],
+    'medium': [0, 0],
+    'high': [0, 0]
+}
 
 for filename in listdir(directory):
 
     im = imread(directory + '/' + filename)
+    im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 
     ksize = 5
 
-    sobel_x = np.abs(Sobel(im, CV_64F, 1, 0, ksize=ksize))
-    sobel_y = np.abs(Sobel(im, CV_64F, 0, 1, ksize=ksize))
+    sobel_x = np.abs(Sobel(im_gray, CV_64F, 1, 0, ksize=ksize))
+    sobel_y = np.abs(Sobel(im_gray, CV_64F, 0, 1, ksize=ksize))
 
     sobel_x *= 255.0 / sobel_x.max()
     sobel_y *= 255.0 / sobel_y.max()
@@ -29,9 +48,20 @@ for filename in listdir(directory):
 
     density = skin.mean() / skin.max()
 
+    shortFilename = filename.split('.')[0]
+    cheering_s = 'cheering' if cheeringness > cheer_threshold else 'idle' 
+    density_s  = 'low' if density < density_low_threshold else ('medium' if density < density_high_threshold else 'high')
+
+    true_density = test_results[shortFilename]['density']
+    if density_s == true_density:
+        density_dict[true_density][0] += 1
+    else:
+        density_dict[true_density][1] += 1
+
+    
     print 'File:', filename
-    print 'Cheeringness: %.4f' % cheeringness
-    print 'Density: %.4f' % density
+    print 'Cheeringness: ',cheering_s, ' ', test_results[shortFilename]['action'],cheeringness
+    print 'Density:',density_s, ' ', true_density, density
     print ''
 
     if filename in display_filenames:
@@ -48,3 +78,19 @@ for filename in listdir(directory):
         title('Skin'), xticks([]), yticks([])
 
         show()
+
+high_total = sum(density_dict['high'])
+medium_total = sum(density_dict['medium'])
+low_total = sum(density_dict['low'])
+
+high_correct = density_dict['high'][0]
+medium_correct = density_dict['medium'][0]
+low_correct = density_dict['low'][0]
+
+high_percent = (1.0*high_correct)/(1.0*high_total) * 100.0
+medium_percent = (1.0*medium_correct)/(1.0*medium_total) * 100.0
+low_percent = (1.0*low_correct)/(1.0*low_total) * 100.0
+
+print 'High Density:', high_correct,'of',high_total,'(%.2f%%' % high_percent,')'
+print 'Medium Density ', medium_correct,'of',medium_total,'(%.2f%%' % medium_percent,')'
+print 'Low Density:', low_correct,'of',low_total,'(%.2f%%' % low_percent,')'
