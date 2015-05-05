e9c26f8df704fb6f9a59af56c17ae7710eefa33a
Sam Adams <samadams532@gmail.com>
2015-05-05 10:18:49 -0500

    resized the categorized images

--- src/resize.py
+++ src/resize.py
@@ -12,25 +12,22 @@ args = vars(ap.parse_args())
 
 images_path = args['images']
 
-image_paths = sorted([
-    os.sep.join([images_path, filename])
-    for filename in os.listdir(images_path)
-    if any(filename.lower().endswith('.{0}'.format(ext)) for ext in ['jpg', 'jpeg', 'png'])
-])
-
 target_width, target_height = (640, 480)
 
-for image_path in image_paths:
-	image = cv2.imread(image_path)
-	height, width, depth = image.shape
-	if (width, height) == (target_width, target_height):
-		continue
-	if 1.0 * width / height > 4.0 / 3.0:
-		image = cv2.resize(image, (width * target_height / height, target_height))
+for root, dirs, files in os.walk(images_path):
+	for filename in files:
+		image_path = os.path.join(root, filename)
+		print image_path
+		image = cv2.imread(image_path)
 		height, width, depth = image.shape
-		image = image[0:target_height, (width / 2 - target_width / 2):(width / 2 + target_width / 2)]
-	else:
-		image = cv2.resize(image, (target_width, height * target_width / width))
-		height, width, depth = image.shape
-		image = image[(height / 2 - target_height / 2):(height / 2 + target_height / 2), 0:target_width]
-	cv2.imwrite(image_path, image)
+		if (width, height) == (target_width, target_height):
+			continue
+		if 1.0 * width / height > 4.0 / 3.0:
+			image = cv2.resize(image, (width * target_height / height, target_height))
+			height, width, depth = image.shape
+			image = image[0:target_height, (width / 2 - target_width / 2):(width / 2 + target_width / 2)]
+		else:
+			image = cv2.resize(image, (target_width, height * target_width / width))
+			height, width, depth = image.shape
+			image = image[(height / 2 - target_height / 2):(height / 2 + target_height / 2), 0:target_width]
+		cv2.imwrite(image_path, image)
