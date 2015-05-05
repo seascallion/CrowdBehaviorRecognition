27f5f6edc7fb34a8dbf15658a811183078818840
Sam Adams <samadams532@gmail.com>
2015-05-05 16:05:45 -0500

    modified old testing system for density and activity to create confusion matrices, and use categorized folder system

--- src/main.py
+++ src/main.py
@@ -1,107 +1,104 @@
+#! /usr/bin/env python
+
 import cv2
 import csv
-from cv2 import imread, Sobel, CV_64F
-from matplotlib.pyplot import title, xticks, yticks, subplot, imshow, show
+from matplotlib import pyplot as plt
 from analysis import analyze
 import numpy as np
-from os import listdir
+import os
 
-display_filenames = []
+display_filenames = ['002.jpg']
 test_results = dict()
 
 cheer_threshold = 0.05
-density_low_threshold = 0.1
-density_high_threshold = 0.18
+density_threshold = 0.08
 
-directory = '../data/images/sampleImages'
-with open('../data/csv.csv') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        test_filename = row['file']
-        test_results[test_filename] = row
+directory = '../data/images/categorized'
 
-density_dict = {
-    'low': [0, 0],
-    'medium': [0, 0],
-    'high': [0, 0]
-}
+already_displayed_filenames = set()
 
-cheering_dict = {
-    'idle': [0, 0],
-    'cheering': [0, 0]
-}
+confusion_dicts = {}
 
-for filename in sorted(listdir(directory)):
+# Dictionary of data per image filename.
+image_data = {}
 
-    im = imread(directory + '/' + filename)
-    cheeringness, density, sobel_x, sobel_y, skin = analyze(im, True)
+for root, dirs, files in os.walk(directory):
+	
+	for filename in files:
+		file_path = os.path.join(root, filename)
+		
+		path_split = root.split(os.sep)
+		parameter = path_split[-2]
+		true_value = path_split[-1]
+		
+		# Predict the value of the density
+		if parameter == 'density':
+			if filename not in image_data:
+				im = cv2.imread(file_path)
+				image_data[filename] = analyze(im, True)
+			cheeringness, density, sobel_x, sobel_y, skin = image_data[filename]
+			predicted_value = 'sparse' if density < density_threshold else 'dense'
+		# Predict the state of the activity
+		elif parameter == 'activity':
+			if filename not in image_data:
+				im = cv2.imread(file_path)
+				image_data[filename] = analyze(im, True)
+			cheeringness, density, sobel_x, sobel_y, skin = image_data[filename]
+			predicted_value = 'cheering' if cheeringness > cheer_threshold else 'idle'
+		else:
+			continue
+		
+		# Increment the count in the confusion matrices.
+		if parameter not in confusion_dicts:
+			confusion_dicts[parameter] = {}
+		if true_value not in confusion_dicts[parameter]:
+			confusion_dicts[parameter][true_value] = {}
+		if predicted_value not in confusion_dicts[parameter][true_value]:
+			confusion_dicts[parameter][true_value][predicted_value] = 0
+		confusion_dicts[parameter][true_value][predicted_value] += 1
+		
+		# Display the image in a window if we set it to.
+		if filename in display_filenames and filename not in already_displayed_filenames:
+			already_displayed_filenames.add(filename)
+			
+			plt.subplot(221), plt.imshow(im, cmap='gray')
+			plt.title('Original Image'), plt.xticks([]), plt.yticks([])
 
-    cheering_s = 'cheering' if cheeringness > cheer_threshold else 'idle'
-    density_s = 'low' if density < density_low_threshold else ('medium' if density < density_high_threshold else 'high')
+			plt.subplot(223), plt.imshow(sobel_x, cmap='gray')
+			plt.title('Sobel X'), plt.xticks([]), plt.yticks([])
 
-    shortFilename = filename.split('.')[0]
-    true_density = test_results[shortFilename]['density']
-    if density_s == true_density:
-        density_dict[true_density][0] += 1
-    else:
-        density_dict[true_density][1] += 1
+			plt.subplot(224), plt.imshow(sobel_y, cmap='gray')
+			plt.title('Sobel Y'), plt.xticks([]), plt.yticks([])
 
-    true_cheer = test_results[shortFilename]['action']
-    if cheering_s == true_cheer:
-        cheering_dict[true_cheer][0] += 1
-    else:
-        cheering_dict[true_cheer][1] += 1
+			plt.subplot(222), plt.imshow(skin, cmap='gray')
+			plt.title('Skin'), plt.xticks([]), plt.yticks([])
 
-    # Delete this sectoin latetr
-    #if true_density == 'high' and density_s != true_density:
-       #display_filenames.append(filename)
+			plt.show()
 
-    print 'File:', filename
-    print 'Cheeringness: ',cheering_s, ' ', test_results[shortFilename]['action'],cheeringness
-    print 'Density:',density_s, ' ', true_density, density
-    print ''
+# Show the raw data for each image
+for filename, data in image_data.items():
+	print 'File:', filename
+	print ' Cheeringness: %0.4f' % data[0]
+	print ' Density:      %0.4f' % data[1]
+	print ''
 
-    if filename in display_filenames:
-        subplot(221), imshow(im, cmap='gray')
-        title('Original Image'), xticks([]), yticks([])
-
-        subplot(223), imshow(sobel_x, cmap='gray')
-        title('Sobel X'), xticks([]), yticks([])
-
-        subplot(224), imshow(sobel_y, cmap='gray')
-        title('Sobel Y'), xticks([]), yticks([])
-
-        subplot(222), imshow(skin, cmap='gray')
-        title('Skin'), xticks([]), yticks([])
-
-        show()
-
-high_total = sum(density_dict['high'])
-medium_total = sum(density_dict['medium'])
-low_total = sum(density_dict['low'])
-
-high_correct = density_dict['high'][0]
-medium_correct = density_dict['medium'][0]
-low_correct = density_dict['low'][0]
-
-high_percent = (1.0*high_correct)/(1.0*high_total) * 100.0
-medium_percent = (1.0*medium_correct)/(1.0*medium_total) * 100.0
-low_percent = (1.0*low_correct)/(1.0*low_total) * 100.0
-
-print 'High Density:', high_correct,'of',high_total,'(%.2f%%' % high_percent,')'
-print 'Medium Density ', medium_correct,'of',medium_total,'(%.2f%%' % medium_percent,')'
-print 'Low Density:', low_correct,'of',low_total,'(%.2f%%' % low_percent,')'
-
-
-idle_total = sum(cheering_dict['idle'])
-cheering_total = sum(cheering_dict['cheering'])
-
-idle_correct = cheering_dict['idle'][0]
-cheering_correct = cheering_dict['cheering'][0]
-
-idle_percent = (1.0*idle_correct)/(1.0*idle_total) * 100.0
-cheering_percent = (1.0*cheering_correct)/(1.0*cheering_total) * 100.0
-
-print ' '
-print 'Idle Activity', idle_correct,'of',idle_total,'(%.2f%%' % idle_percent,')'
-print 'Cheering Activity', cheering_correct,'of',cheering_total,'(%.2f%%' % cheering_percent,')'
+# Show the confusion matrices with accuracy proportions
+for parameter, confusion_dict in confusion_dicts.items():
+	values = sorted(confusion_dict.keys())
+	print 'Confusion matrix for %s:' % parameter
+	print ''
+	print '               PREDICTED'
+	print 'KNOWN     | ' + ''.join(value.ljust(10) for value in values)
+	print '-' * 10 + '-' + '-' * 10 * len(values)
+	for value in values:
+		predicted = confusion_dict[value]
+		counts = [predicted[val] if val in predicted else 0 for val in values]
+		total_count = sum(counts)
+		proportions = [1.0 * count / total_count for count in counts]
+		print value.ljust(10) + '|' + ''.join(('%0.2f' % prop).ljust(10) for prop in proportions)
+	print ''
+	total = sum(sum(prediction_vec.values()) for prediction_vec in confusion_dict.values())
+	total_correct = sum(confusion_dict[value][value] for value in confusion_dict.keys())
+	accuracy = 1.0 * total_correct / total
+	print '* Average Accuracy is %0.2f' % accuracy
+	print ''
