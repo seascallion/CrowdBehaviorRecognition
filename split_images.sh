9502fd082fd74413ec5fe8f58359cb7eb96c76b5
Sam Adams <samadams532@gmail.com>
2015-05-04 19:11:59 -0500

    made matlab script to test BoF behavior classification

--- split_images.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-python src/split_image.py data/images/hands/all/positive.png 8 8 data/images/hands/positive
-python src/split_image.py data/images/hands/all/negative.png 8 8 data/images/hands/negative
